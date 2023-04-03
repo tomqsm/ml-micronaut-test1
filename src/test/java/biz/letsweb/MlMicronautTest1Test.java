40c2b2cd369ea32b8b6e2973555ee7f0122837b7
Tomasz <tomasz.kusmierczyk@letsweb.biz>
2023-04-03 17:59:42 +0000

    Initial commit

---
+++ src/test/java/biz/letsweb/MlMicronautTest1Test.java
@@ -0,0 +1,21 @@
+package biz.letsweb;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class MlMicronautTest1Test {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
